eaa5e117e7a39bb996917ccd2bfe50bac2a41c57
wqr1 <dimakond520@gmail.com>
2024-07-25 00:12:19 +0400

    Edited error handler on fetch

--- src/components/AnimeCardCarousel.tsx
+++ src/components/AnimeCardCarousel.tsx
@@ -1,5 +1,3 @@
-import axios from "axios";
-
 import AnimeCard from "@/components/AnimeCard";
 import Arrows from "@/components/Arrows";
 
@@ -7,28 +5,31 @@ import { AnimeData } from "@/util/AnimeDataType";
 
 import styles from "@/styles/AnimeCardCarousel.module.css";
 
-const getAnimeData = async (host: string, url: string) => {
-  const res = await axios.get(`${host}${url}`);
-  return res.data;
+const getAnimeData = async (url: string) => {
+  const res = await fetch(`https://api.anilibria.tv/v3/${url}`);
+  return await res.json();
 };
 
-const AnimeCardCarousel = async (props: { url: string; idKey: string }) => {
-  const data: { list: AnimeData[] } = await getAnimeData(
-    "https://api.anilibria.tv/v3/",
-    props.url,
-  );
+const AnimeCardCarousel = async ({
+  url,
+  idKey,
+}: {
+  url: string;
+  idKey: string;
+}) => {
+  const data: { list: AnimeData[] } = await getAnimeData(url);
 
   return (
-    <div key={props.idKey} className="relative">
+    <div key={idKey} className="relative">
       <div
-        id={`${props.idKey}-anime-card-carousel-scroll`}
+        id={`${idKey}-anime-card-carousel-scroll`}
         className={`${styles.anime_card_carousel_scroll} flex w-[100%] snap-x snap-mandatory gap-4 overflow-scroll`}
       >
         {data.list.map((item, idx) => (
-          <AnimeCard key={`${props.idKey}-anime-card-${idx}`} data={item} />
+          <AnimeCard key={`${idKey}-anime-card-${idx}`} data={item} />
         ))}
       </div>
-      <Arrows idKey={props.idKey} />
+      <Arrows idKey={idKey} />
       <div className={`${styles.shadow} left-0`}></div>
       <div className={`${styles.shadow} right-0`}></div>
     </div>
